b807102f21b211dad40564d63fa61110bd8ebbdf
zhangyanyu <zhangyanyu2018@gmail.com>
2019-01-06 16:04:35 +0800

    modify response form

--- script/springboot/getdynamiclist.sh
+++ script/springboot/getdynamiclist.sh
@@ -1,6 +1,5 @@
 userid=$1
 curl  --header 'Content-Type: application/json'\
-    --request POST\
-    --data '{"userid":"'$userid'"}' \
-	http://localhost:8888/Dynamic/getList
+    --request GET\
+	http://localhost:8888/Dynamic/getList?userid=${userid}
 read -s -n1 -p "press any key to continue... "
